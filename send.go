15950b85ac83a8284038b5bf682e51fbc72c15b7
milo <shiguangwangyu@gmail.com>
2026-09-24 16:05:22 +0800

    fix:消息心情不在判断群权限

--- send.go
+++ send.go
@@ -302,9 +302,12 @@ func (cli *Client) SendMessage(ctx context.Context, to types.JID, message *waE2E
 	if to.Server == types.GroupServer || to.Server == types.BroadcastServer {
 		start := time.Now()
 		if to.Server == types.GroupServer {
-			err = cli.checkStoredGroupSendPermission(ctx, to)
-			if err != nil {
-				return
+			// Reactions are allowed even when only admins can post to the group.
+			if getTypeFromMessage(message) != "reaction" {
+				err = cli.checkStoredGroupSendPermission(ctx, to)
+				if err != nil {
+					return
+				}
 			}
 			var cachedData *groupMetaCache
 			cachedData, err = cli.getCachedGroupData(ctx, to)
